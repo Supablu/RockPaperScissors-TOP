6a541f97664619b6b4b43ed92a7b49c02689490d
Luis del Valle <luis.delvalle0124@gmail.com>
2022-09-13 11:26:09 -0700

    Created determine winner function and gameround. Tinkering on how to track games of 5

--- script.js
+++ script.js
@@ -23,12 +23,34 @@ function getComputerChoice() {
 }
 
 //determine who wins
-function determineWinner() {
-
+function determineWinner(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return 'Draw!';
+    } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
+        return 'Player wins!';
+    } else if (playerChoice === 'paper' && computerChoice === 'rock') {
+        return 'Player wins!';
+    } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+        return 'Player wins!';
+    } else {
+        return 'Computer wins!';
+    }
 }
 
 
 //single round of the game
 function gameRound() {
+    const playerChoice = getPlayerChoice();
+    const computerChoice = getComputerChoice();
+    console.log(`You chose ${playerChoice}!`);
+    console.log(`The computer chose ${computerChoice}!`);
 
+    console.log(determineWinner(playerChoice, computerChoice));
+}
+
+//Play 5 rounds
+function game() {
+    for (let i = 0; i < 5; i++) {
+        return gameRound();
+    }
 }
