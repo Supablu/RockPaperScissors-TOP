31477f46329139c090dd618f818e7a7cc1cc825b
Luis del Valle <luis.delvalle0124@gmail.com>
2022-09-12 18:54:55 -0700

    Coded computer choice function

---
+++ script.js
@@ -0,0 +1,14 @@
+//Global variable for the only 3 valid answers for user
+const choices = ['rock', 'paper', 'scissors'];
+
+//Generate computer choice
+function getComputerChoice() {
+    let randomChoice = Math.floor(Math.random() * 3) + 1;
+    if (randomChoice === 1) {
+        return 'rock';
+    } else if (randomChoice === 2) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
